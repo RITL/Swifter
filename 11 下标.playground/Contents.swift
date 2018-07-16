c1d65c3096f9fe70d949c081c3a80179ae611858
774655048@qq.com <774655048@qq.com>
2018-07-16 16:53:49 +0800

    update 11

--- 11 下标.playground/Contents.swift
+++ 11 下标.playground/Contents.swift
@@ -5,14 +5,12 @@ import UIKit
 // 在Swift中，Array 和 Dictionary 实现了下标读写
 
 var arr = [1,2,3]
-arr[2]      // 3
-arr[2] = 4  // arr = [1,2,4]
-
-print("\(arr.enumerated())")
+//let _ = arr[2]      // 3
+//arr[2] = 4  // arr = [1,2,4]
 
 
 var dic = ["cat":"meow","goat":"mie"]
-dic["cat"]    // { Some "meow" }
+let _ = dic["cat"]    // { Some "meow" }
 dic["car"] = "miao" // dic = ["cat":"miao","goat":"mie"]
 
 // 字典通过下标访问到的结果是 一个Optional的值
@@ -26,8 +24,8 @@ dic["car"] = "miao" // dic = ["cat":"miao","goat":"mie"]
 // 需求:
 // 很难一次性的去除某几个特定位置的元素
 // 比如在一个数组内，想去除index为0，2，3的元素。
-// 很可鞥我们就会要去枚举数组，然后在循环里判断是否使我们想要的位置。
-// 实现一个接受数组标作为下不熬输入的读取方法
+// 很可能我们就会要去枚举数组，然后在循环里判断是否使我们想要的位置。
+// 实现一个接受数组标作为下标输入的读取方法
 
 extension Array {
     subscript(input: [Int]) -> ArraySlice<Element>{
@@ -50,11 +48,8 @@ extension Array {
 }
 
 var arr1 = [1,2,3,4,5]
-
-//Playground就是报错 - - Swifter项目中可以
-
-//let _ = arr1[[0,2,3]] // [1,3,4]
-//arr1[[0,2,3]] = [-1,-3,-4] // [-1, 2, -3, -4, 5]
+let _ = arr1[[0,2,3]] // [1,3,4]
+arr1[[0,2,3]] = [-1,-3,-4] // [-1, 2, -3, -4, 5]
 
 // 极力不推荐如此使用！！！
 
