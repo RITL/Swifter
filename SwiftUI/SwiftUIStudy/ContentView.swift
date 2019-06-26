86acfbeb24ffc0a2644f6574427fb1fb0623e15d
岳文 <yuexiaowen108@gmail.com>
2019-06-26 16:06:31 +0800

    add SwiftUI

---
+++ SwiftUI/SwiftUIStudy/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIStudy
+//
+//  Created by YueWen on 2019/6/26.
+//  Copyright © 2019 YueWen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
