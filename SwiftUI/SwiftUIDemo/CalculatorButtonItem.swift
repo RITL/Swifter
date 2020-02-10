0b687a37bd65b2d83be619eb6d660c74d82bad61
岳文 <yuexiaowen108@gmail.com>
2020-02-10 17:05:27 +0800

    update 使用ForEach 重构

--- SwiftUI/SwiftUIDemo/CalculatorButtonItem.swift
+++ SwiftUI/SwiftUIDemo/CalculatorButtonItem.swift
@@ -2,62 +2,69 @@
 //  CalculatorButtonItem.swift
 //  SwiftUIDemo
 //
-//  Created by YueWen on 2019/9/3.
-//  Copyright © 2019 YueWen. All rights reserved.
+//  Created by YueWen on 2020/1/15.
+//  Copyright © 2020 YueWen. All rights reserved.
 //
 
 import Foundation
 import CoreGraphics
 
-
+/// 按钮类型
 enum CalculatorButtonItem {
     
+    /// 运算符
     enum Op: String {
         case plus = "+"
         case minus = "-"
-        case divde = "/"
+        case divide = "/"
         case multiply = "x"
         case equal = "="
     }
     
+    /// 命令按钮
     enum Command: String {
-        case clear = "AC"
+        case clear = "A/C"
         case flip = "+/-"
-        case precent = "%"
+        case percent = "%"
     }
     
+    /// 数字按钮
     case digit(Int)
+    /// 小数点
     case dot
+    /// 操作符
     case op(Op)
+    /// 命令按钮
     case command(Command)
 }
 
-extension CalculatorButtonItem: Hashable {}
 
 
 extension CalculatorButtonItem {
     
+    // 显示的文本
     var title: String {
         switch self {
-        case .digit(let value): return String(value)
+        case .digit(let value): return "\(value)"
         case .dot: return "."
         case .op(let op): return op.rawValue
         case .command(let command): return command.rawValue
         }
     }
     
+    // 按钮大小
     var size: CGSize {
-        if case .digit(let value) = self, value == 0 {
-            return CGSize(width: 88 * 2 + 8, height: 88)
-        }
-        return CGSize(width: 88, height: 88)
+        CGSize(width: 88, height: 88)
     }
     
-    var backgroundColorName: String {
+    // 背景色名称
+    var backgroundName: String {
         switch self {
-        case .digit,.dot: return "digitBackground"
+        case .digit, .dot: return "digitBackground"
         case .op: return "operatorBackground"
         case .command: return "commandBackground"
         }
     }
 }
+
+extension CalculatorButtonItem: Hashable {}
